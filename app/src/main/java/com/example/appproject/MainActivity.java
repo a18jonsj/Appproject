0e55a7d38b409fc551968b38d8963039da6954a2
a18jonsj <a18jonsj@student.his.se>
2019-04-30 15:50:54 +0200

    Bättre listview, måste fixa tooolbar.

--- app/src/main/java/com/example/appproject/MainActivity.java
+++ app/src/main/java/com/example/appproject/MainActivity.java
@@ -77,14 +77,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id =item.getItemId();
+
+        if ( (id==R.id.action_settings)){
+            return true;
+        }
+
+        if ( (id==R.id.action_refresh)){
+            new FetchData().execute();
             return true;
         }
 
